a955cb8253dc6dd156136695d0f29e9908331868
Aidana <askaribayeva@gmail.com>
2015-11-08 12:54:31 +0000

    Former-commit-id: a8e3aabc675368d1a54bec5be85b9f1fbcfc8228

--- dev/testvoc1/testvoc.sh
+++
@@ -1,6 +0,0 @@
-if [[ $1 = "kaz-eng" ]]; then
-echo "==Kazakh->English===========================";
-bash inconsistency.sh kaz-eng > /tmp/kaz-eng.testvoc; bash inconsistency-summary.sh /tmp/kaz-eng.testvoc kaz-eng
-echo ""
-
-fi
